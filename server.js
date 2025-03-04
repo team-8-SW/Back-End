e692bbf2f8ff88a577d14c97b32c7a1dc069f134
Abdallah Mohamed ElMahdy <abdallahmohamed3069@gmail.com>
2025-03-04 23:15:50 +0200

    "lloool"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app.js');
 const dotenv = require('dotenv');
 dotenv.config({ path:  './config.env' });
 
-
+console.log(process.env);
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
