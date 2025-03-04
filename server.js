2b0d43a4ede3f268cb8183f99e242dc9e1333f02
Abdallah Mohamed ElMahdy <abdallahmohamed3069@gmail.com>
2025-03-04 22:25:39 +0200

    Intial Project Structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app.js');
+const dotenv = require('dotenv');
+dotenv.config({ path:  './config.env' });
+
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
