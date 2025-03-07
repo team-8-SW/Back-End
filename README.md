94545dbd7f3b931e8118e49899562751a8cd3c87
Abdallah Mohamed ElMahdy <abdallahmohamed3069@gmail.com>
2025-03-07 19:37:55 +0200

    "final2"

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Back-End
 # You need to run these command in terminal (npm i express nodemon pg)
 # Install dotenv extension and then run in terminal (npm install dotenv)
-# To run it (npm run start)
+# To run it (npm run live)
 # npm install --save-dev @types/swagger-ui-express @types/swagger-jsdoc
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-const app = require('./app.js');
-const dotenv = require('dotenv');
+import app from './app.js';
+import dotenv from 'dotenv';
 dotenv.config({ path:  './config.env' });
 
 console.log(process.env);
@@ -7,3 +7,10 @@ console.log(process.env);
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+
+
+
+
+
+
