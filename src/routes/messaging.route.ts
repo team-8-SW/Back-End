60ea492139c715fa54bf54747e798f82c6701376
JuliaEmadMk <151572528+JuliaEmadMk@users.noreply.github.com>
2025-04-01 15:52:51 +0200

    Send media (images, videos, documents) in messages

--- src/routes/messaging.route.ts
+++ src/routes/messaging.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { sendTextMessage } from '../controllers/messaging.controller';
+import { upload } from '../middleware/multer';
+import { sendTextMessage, sendMediaMessage } from '../controllers/messaging.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
+// POST /api/messages
 router.post('/messages', authMiddleware, sendTextMessage);
+
+// POST /api/messages/media
+router.post('/messages/media', authMiddleware, upload.single('media'), sendMediaMessage);
 
 export default router;
