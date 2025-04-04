dff7210b83fefff26c99237002734f288edeafc9
Abdallah Mohamed ElMahdy <abdallahmohamed3069@gmail.com>
2025-04-04 02:56:00 +0200

    Creating company

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -23,9 +23,14 @@ export const getCompanyById = async (req: Request, res: Response) => {
 export const createCompany = async (req: Request, res: Response) => {
 	try {
 		const companyData = req.body;
-		const newCompany = await companyService.createCompany(companyData);
-		res.status(201).json(newCompany);
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		const admin_user_id = (req as any).user?.user_id;
+		if (!admin_user_id) return res.status(401).json({ message: 'Unauthorized' });
+
+		const newCompany = await companyService.createCompany({ ...companyData, admin_user_id });
+		res.status(201).json({ message: 'Company created successfully', company: newCompany });
 	} catch (error) {
+		console.error('Error creating company:', error);
 		res.status(500).json({ error: 'Internal server Error' });
 	}
 };
--- src/routes/company.route.ts
+++ src/routes/company.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as companyController from '../controllers/company.controller';
 // import upload from '../middleware/upload.middleware';
+import { newAuthMiddleware } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
 router.get('/', companyController.getAllCompanies);
 router.get('/:id', companyController.getCompanyById);
-router.post('/', companyController.createCompany);
+router.post('/', newAuthMiddleware, companyController.createCompany);
 router.put('/:id', companyController.updateCompany);
 
 export default router;
