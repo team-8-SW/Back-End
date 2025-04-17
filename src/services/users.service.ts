3646679e25eedead95ade0b5c80512e955690b0d
noortantawy <noor.khalil04@eng-st.cu.edu.eg>
2025-04-18 00:36:11 +0200

    haga fi search

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -116,6 +116,8 @@ export const searchUsers = async (
 					);
 				});
 		});
+		// Exclude the user sending the request
+			usersQuery.andWhere('users.id', '!=', userId);
 	}
 
 	// Apply search filter for name
