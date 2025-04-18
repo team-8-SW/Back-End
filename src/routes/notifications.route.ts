16eb0a38da37fd62bccfd2783e8b4c91db22a750
noortantawy <noor.khalil04@eng-st.cu.edu.eg>
2025-04-18 13:54:05 +0200

    notification edit route

--- src/routes/notifications.route.ts
+++ src/routes/notifications.route.ts
@@ -6,5 +6,5 @@ const router = express.Router();
 
 router.get('/me', newAuthMiddleware, notificationsController.getAllNotifications);
 router.get('/me/unread-count', newAuthMiddleware, notificationsController.getUnreadCount);
-router.put('/me/:id/markasread', newAuthMiddleware, notificationsController.markNotificationAsRead);
+router.patch('/me/:id/markasread', newAuthMiddleware, notificationsController.markNotificationAsRead);
 export default router;
