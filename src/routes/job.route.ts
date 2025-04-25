ce7fd18c2f3e091422a979bb5f9e267e5fe7d6eb
Abdallah Mohamed ElMahdy <abdallahmohamed3069@gmail.com>
2025-04-25 15:19:24 +0300

    YARAB A5LS YA MANSI

--- src/routes/job.route.ts
+++ src/routes/job.route.ts
@@ -6,18 +6,20 @@ import { newAuthMiddleware } from '../middleware/auth.middleware';
 const router = express.Router();
 
 router.get('/employer', newAuthMiddleware, jobController.getJobsByUserId);
-router.get('/:job_id', jobController.getApplicationsByJobId);
-router.get('/', jobController.getJobs);
 router.get('/applicant', newAuthMiddleware, jobController.getJobsByApplicant);
 router.get('/search', jobController.searchJob);
 router.get('/filter', jobController.filterJob);
-router.post('/:id/save', newAuthMiddleware, jobController.saveJob);
-router.delete('/:id/unsave', newAuthMiddleware, jobController.unSaveJob);
 router.post('/post-job', newAuthMiddleware, jobController.postJob);
 
+router.get('/:job_id', jobController.getApplicationsByJobId);
+
+router.post('/:id/save', newAuthMiddleware, jobController.saveJob);
+router.delete('/:id/unsave', newAuthMiddleware, jobController.unSaveJob);
 router.post('/:id/apply', newAuthMiddleware, jobController.applyForJob);
 router.get('/:id/status', newAuthMiddleware, jobController.getStatus);
 
 router.get('/:id', jobController.getJobById);
+router.get('/', jobController.getJobs);
+
 
 export default router;
